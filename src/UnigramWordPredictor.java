bfee4098f1479592dd851a3ba9331dd9c8a27961
Jake-G123 <jakelgerken@gmail.com>
2025-10-14 19:27:13 -0700

    Completed wave 5

--- src/UnigramWordPredictor.java
+++ src/UnigramWordPredictor.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -125,7 +126,10 @@ public class UnigramWordPredictor implements WordPredictor {
   public String predictNextWord(List<String> context) {
     // TODO: Return a predicted word given the words preceding it
     // Hint: only the last word in context should be looked at
-    return null;
+    List<String> potentialTokens = new ArrayList<>(neighborMap.get(context.getLast()));
+    Random random = new Random();
+    int tokenID = random.nextInt(potentialTokens.size());
+    return potentialTokens.get(tokenID);
   }
   
   /**
